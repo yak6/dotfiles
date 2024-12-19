fd9bd78d258c4209b85d773ef87e4574d8d00fb4
Antoni Dobrowolski <85031661+yak6@users.noreply.github.com>
2024-12-19 15:20:22 +0100

    small updates
    - change theme to OneDark
    - added some basic vim settings
    - change cursor to block when quitting

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,48 +1,57 @@
+-- yak6's neovim config
+
 -- Packer bootstrap
 vim.cmd [[packadd packer.nvim]]
 
 require('packer').startup(function(use)
-    -- Update the packer
+    -- Update Packer
     use 'wbthomason/packer.nvim'
-    
+
     -- Plugins
     use {
         "windwp/nvim-autopairs",
         config = function()
             require("nvim-autopairs").setup({
-                check_ts = true, 
+                check_ts = true,
                 disable_filetype = { "TelescopePrompt", "vim" },
             })
         end
     }
-    -- Themes  
-    use { "catppuccin/nvim", as = "catppuccin" }
-
+    -- Themes
+    use 'navarasu/onedark.nvim'
 end)
 
-vim.cmd.colorscheme "catppuccin"
+require('onedark').setup {
+    style = 'darker'
+}
+require('onedark').load()
 
--- Basic settings
-vim.opt.number = true           
+-- Settings
+vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.cursorline = true
 
-vim.opt.shiftwidth = 4      
-vim.opt.tabstop = 4        
-vim.opt.expandtab = true    
-vim.opt.autoindent = true   
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
 
-vim.opt.hlsearch = true     
-vim.opt.fillchars = { eob = ' ' }  
+vim.opt.backup = false
+vim.opt.swapfile = false
+vim.opt.ignorecase = true
+vim.opt.hlsearch = true
+vim.opt.fillchars = { eob = ' ' }
+
 vim.opt.wildignore = '*.docx,*.jpg,*.png,*.gif,*.pdf,*.pyc,*.exe,*.flv,*.img,*.xlsx'
 
 vim.opt.encoding = 'utf-8'
 vim.opt.fileencoding = 'utf-8'
 
+vim.api.nvim_create_augroup('CursorSettings', { clear = true })
 
--- Restore cursor on nvim exit
-vim.api.nvim_create_autocmd('VimLeave', {
-  callback = function()
-    vim.opt.guicursor = 'a:ver25'
-  end,
+vim.api.nvim_create_autocmd('VimLeave', { -- After leaving NeoVim, change to block cursor shape
+  group = 'CursorSettings',
+  pattern = '*',
+  command = 'echo -e "\\033[0 q"',
 })
