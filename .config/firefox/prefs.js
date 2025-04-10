e7b205af493efd788880336dbedf7c166c83f316
yak6 <85031661+yak6@users.noreply.github.com>
2025-04-10 21:59:33 +0200

    Upload prefs.js

---
+++ .config/firefox/prefs.js
@@ -0,0 +1,11 @@
+// Do not prefil forms:
+user_pref("signon.prefillForms", false);
+
+// Disable push notifications:
+user_pref("dom.push.enabled", false);
+
+// Disable alt key to access menu (using alt as super key in i3)
+user_pref("ui.key.menuAccessKey", -1)
+
+// Enable custom userChrome.css (this feature will make chrome/userChrome.css work):
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
