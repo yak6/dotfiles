3dec282d54ae075ec5529785b3fdf2aa754956cd
yak6 <85031661+yak6@users.noreply.github.com>
2025-05-27 19:33:25 +0200

    Create aliases.zsh

---
+++ .zsh/aliases.zsh
@@ -0,0 +1,24 @@
+# Output with colors
+alias ls='ls --color' 
+alias grep='grep --color' 
+alias diff='diff --color' 
+alias ip='ip --color'
+
+# Listing files
+alias l='ls -CF' 
+alias ll='ls -lh'
+alias la='ls -A'
+
+# File operations
+alias cp='cp -iv' 
+alias mv='mv -iv' 
+alias rm='rm -vI' 
+alias mkdir='mkdir -pv'
+
+# Shortcuts
+alias nv="nvim" 
+alias s="sudo" 
+alias v="vim" 
+alias g="git" 
+alias sv="sudo vim" 
+alias reload="source ~/.zshrc"
